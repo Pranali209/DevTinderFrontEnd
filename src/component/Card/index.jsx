0de0c99f33b92cd9e45d425c69584c9772bb9bae
Pranali <pgmodgekar99@gmail.com>
2025-06-02 20:53:57 +0530

    like and dislike symabols added

--- src/component/Card/index.jsx
+++ src/component/Card/index.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { motion, useMotionValue, useTransform } from "framer-motion";
+import { motion, useMotionValue, useTransform , useMotionValueEvent } from "framer-motion";
 import skillIcon from '../../assets/skill.png';
 import axios from 'axios';
 import { Base_Url } from '../../utlis/Contant';
@@ -11,8 +11,8 @@ const Card = () => {
   const dispatch = useDispatch()
   const data = useSelector(store => store.feed);
   const [cards, setCards] = React.useState([]);
+  const [dragDirection, setDragDirection] = useState(null);
 
-  const [liked, setLiked] = useState('');
 
   useEffect(() => {
     setCards(data?.data);
@@ -23,7 +23,16 @@ const Card = () => {
   const x = useMotionValue(0);
   const rotateRaw = useTransform(x, [-150, 150], [-18, 18]);
   const opacity = useTransform(x, [-150, 0, 150], [0, 1, 0]);
-
+  
+   useMotionValueEvent(x, "change", (latest) => {
+    if (latest > 100) {
+      setDragDirection("right");
+    } else if (latest < -100) {
+      setDragDirection("left");
+    } else {
+      setDragDirection(null);
+    }
+  })
 
  
   const handleDragEnd = (id) => {
@@ -120,6 +129,8 @@ const Card = () => {
         }}
         onDragEnd={() => handleDragEnd(item._id)}
       >
+       
+     
         <div key={index}
           className="product-card w-[320px] h-[500px] rounded-md shadow-xl overflow-hidden z-[100] relative cursor-pointer snap-start shrink-0 py-8 px-6 bg-white flex flex-col items-center justify-center gap-3 transition-all duration-300 group">
           <div className="absolute -left-[40%] top-0 group-hover:rotate-12 transition-all duration-300 group-hover:scale-150">
@@ -167,6 +178,18 @@ const Card = () => {
             </div>
           </div>
         </div>
+        {
+         dragDirection === "right"  ? (
+           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" 
+           className="absolute w-[50px] h-[50px] top-56 -right-20 fill-blue-500">
+        <path d="M313.4 32.9c26 5.2 42.9 30.5 37.7 56.5l-2.3 11.4c-5.3 26.7-15.1 52.1-28.8 75.2l144 0c26.5 0 48 21.5 48 48c0 18.5-10.5 34.6-25.9 42.6C497 275.4 504 288.9 504 304c0 23.4-16.8 42.9-38.9 47.1c4.4 7.3 6.9 15.8 6.9 24.9c0 21.3-13.9 39.4-33.1 45.6c.7 3.3 1.1 6.8 1.1 10.4c0 26.5-21.5 48-48 48l-97.5 0c-19 0-37.5-5.6-53.3-16.1l-38.5-25.7C176 420.4 160
+         390.4 160 358.3l0-38.3 0-48 0-24.9c0-29.2 13.3-56.7 36-75l7.4-5.9c26.5-21.2 
+         44.6-51 51.2-84.2l2.3-11.4c5.2-26 30.5-42.9 56.5-37.7zM32 192l64 0c17.7 0 32 14.3 32 32l0 224c0 17.7-14.3 32-32 32l-64 0c-17.7 0-32-14.3-32-32L0 224c0-17.7 14.3-32 32-32z"/></svg>)
+         : dragDirection === "left" ? <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"
+          className="absolute w-[50px] h-[50px] top-56 -left-20 fill-blue-500">
+        <path d="M313.4 479.1c26-5.2 42.9-30.5 37.7-56.5l-2.3-11.4c-5.3-26.7-15.1-52.1-28.8-75.2l144 0c26.5 0 48-21.5 48-48c0-18.5-10.5-34.6-25.9-42.6C497 236.6 504 223.1 504 208c0-23.4-16.8-42.9-38.9-47.1c4.4-7.3 6.9-15.8 6.9-24.9c0-21.3-13.9-39.4-33.1-45.6c.7-3.3 1.1-6.8 1.1-10.4c0-26.5-21.5-48-48-48l-97.5 0c-19 0-37.5 5.6-53.3 16.1L202.7 73.8C176 91.6 160 121.6 160 153.7l0 38.3 0 48 0 24.9c0 29.2 13.3 56.7 36 75l7.4 5.9c26.5 21.2 44.6 51 51.2 84.2l2.3 11.4c5.2 26 30.5 42.9 56.5 37.7zM32 384l64 0c17.7 0 32-14.3 32-32l0-224c0-17.7-14.3-32-32-32L32 96C14.3 96 0 110.3 0 128L0 352c0 17.7 14.3 32 32 32z"/></svg>
+        :""
+       }
       </motion.section>
     ))
   );
